c064812e07b36c340a26d88ab488feeeb3dc3d63
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-09 22:30:58 +0100

    Fix commenti Doxygen e implementazione metodi

--- src/main/java/Models/Prestiti/Prestito.java
+++ src/main/java/Models/Prestiti/Prestito.java
@@ -7,44 +7,46 @@ import java.time.LocalDate;
 
 /**
  * @class Prestito
- * @brief Classe che rappresenta un prestito di un libro da parte di un utente.
+ * @brief Classe che rappresenta un prestito di un libro effettuato ad un
+ *        utente.
  */
 public class Prestito implements Comparable<Prestito> {
-    private final Matricola matricola;
-    private final ISBN id;
+    private final Matricola matricolaUtente;
+    private final ISBN codiceISBNLibro;
     private final LocalDate dataInizio;
     private LocalDate dataScadenza;
     private LocalDate dataRestituzione;
 
     /**
      * @brief Costruttore.
-     * @param[in] matricola Matricola dell'utente che effettua il prestito.
-     * @param[in] id ISBN del libro prestato.
+     * @param[in] matricolaUtente Matricola dell'utente che ha effettuato il
+     *            prestito.
+     * @param[in] codiceISBNLibro ISBN del libro prestato.
      * @param[in] dataInizio Data di inizio del prestito.
      * @param[in] dataScadenza Data di scadenza del prestito.
-    */
-    public Prestito(Matricola matricola, ISBN id, LocalDate dataInizio, LocalDate dataScadenza) {
-        this.matricola = matricola;
-        this.id = id;
+     */
+    public Prestito(Matricola matricolaUtente, ISBN codiceISBNLibro, LocalDate dataInizio, LocalDate dataScadenza) {
+        this.matricolaUtente = matricolaUtente;
+        this.codiceISBNLibro = codiceISBNLibro;
         this.dataInizio = dataInizio;
         this.dataScadenza = dataScadenza;
-        this.dataRestituzione = null;
+        dataRestituzione = null;
     }
 
     /**
-     * @brief Imposta la data di scadenza del prestito.
-     * @param[in] data Data di scadenza da impostare.
+     * @brief Restituisce la matricola dell'utente che ha effettuato il prestito.
+     * @return Matricola dell'utente che ha effettuato il prestito.
      */
-    public void setDataScadenza(LocalDate data) {
-        this.dataScadenza = data;
+    public Matricola getMatricolaUtente() {
+        return matricolaUtente;
     }
 
     /**
-     * @brief Restituisce la data di scadenza del prestito.
-     * @return Data di scadenza del prestito.
+     * @brief Restituisce l'ISBN del libro prestato.
+     * @return ISBN del libro prestato.
      */
-    public LocalDate getDataScadenza() {
-        return dataScadenza;
+    public ISBN getCodiceISBNLibro() {
+        return codiceISBNLibro;
     }
 
     /**
@@ -56,11 +58,11 @@ public class Prestito implements Comparable<Prestito> {
     }
 
     /**
-     * @brief Imposta la data di restituzione del prestito.
-     * @param[in] data Data di restituzione da impostare.
+     * @brief Restituisce la data di scadenza del prestito.
+     * @return Data di scadenza del prestito.
      */
-    public void setDataRestituzione(LocalDate data) {
-        this.dataRestituzione = data;
+    public LocalDate getDataScadenza() {
+        return dataScadenza;
     }
 
     /**
@@ -72,41 +74,51 @@ public class Prestito implements Comparable<Prestito> {
     }
 
     /**
+     * @brief Imposta la data di scadenza del prestito.
+     * @param[in] data Data di scadenza da impostare.
+     */
+    public void setDataScadenza(LocalDate data) {
+        dataScadenza = data;
+    }
+
+    /**
+     * @brief Imposta la data di restituzione del prestito.
+     * @param[in] data Data di restituzione da impostare.
+     */
+    public void setDataRestituzione(LocalDate data) {
+        dataRestituzione = data;
+    }
+
+    /**
      * @brief Verifica se il prestito è attivo.
      * @return true se il prestito è attivo, false altrimenti.
      */
-    public boolean isAttivo(){
-        return false;
+    public boolean isAttivo() {
+        return (dataRestituzione != null);
     }
 
     /**
-     * @brief Verifica se la data di scadenza è passata.
+     * @brief Verifica se la data di scadenza inserita è valida.
      * @param[in] dataScadenza Data di scadenza da verificare.
-     * @return true se la data di scadenza è passata, false altrimenti.
+     * @return true se la data di scadenza inserita è logicamente valida (ovvero
+     *         successiva alla data corrente), false
+     *         altrimenti.
      */
     public static boolean verificaDataScadenza(LocalDate dataScadenza) {
         LocalDate today = LocalDate.now();
-        return dataScadenza.isBefore(today);
+        return dataScadenza.isAfter(today);
     }
 
     /**
-     * @brief Verifica l'uguaglianza tra due prestiti.
-     * @param[in] o Oggetto da confrontare.
-     * @return true se i prestiti sono uguali, false altrimenti.
+     * @brief Confronta due prestiti in base alla loro data di scadenza.
+     * @param[in] p Prestito da confrontare col prestito corrente.
+     * @return Valore minore di zero, pari a zero oppure maggiore di zero se la data
+     *         di scadenza del prestito corrente
+     *         è rispettivamente precedente, uguale o successiva alla data di
+     *         scadenza del prestito p.
      */
     @Override
-    public boolean equals(Object o){
-        return false;
-    }
-
-    /**
-     * @brief Confronta due prestiti in base alla data di scadenza.
-     * @param[in] p Prestito da confrontare.
-     * @return Valore negativo, zero o positivo se questo prestito è rispettivamente
-     *         minore, uguale o maggiore del prestito p.
-     */
-    @Override
-    public int compareTo(Prestito p){
+    public int compareTo(Prestito p) {
         return 0;
     }
 }
