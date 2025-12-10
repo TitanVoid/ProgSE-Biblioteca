b858b66d9347efc6b08b6a6787db031dbd5ef972
Francesco Altieri <fra04alt@gmail.com>
2025-12-10 22:00:15 +0100

    modifica BaseController

--- src/main/java/controllers/BaseController.java
+++ src/main/java/controllers/BaseController.java
@@ -1,10 +1,17 @@
 package controllers;
 
+import controllers.libri.LibriController;
 import javafx.scene.control.Alert;
 import models.Biblioteca;
 
 public abstract class BaseController {
     protected Biblioteca biblioteca;
+    private BaseController parentController;
+
+    private void setParentController(BaseController parentController) {
+        this.parentController = parentController;
+    };
+
 
     public void setBiblioteca(Biblioteca biblioteca) {
         this.biblioteca = biblioteca;
