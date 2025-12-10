5f607d36949c6b2ab5d2f40fd45a0a3185983251
Francesco Altieri <fra04alt@gmail.com>
2025-12-10 22:40:57 +0100

    aggiunta aggiuntaLibroController

--- src/main/java/controllers/libri/AggiuntaLibroController.java
+++ src/main/java/controllers/libri/AggiuntaLibroController.java
@@ -3,10 +3,20 @@ import controllers.BaseController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import models.Biblioteca;
+import models.ISBN;
+import models.libri.Autore;
+import models.libri.Libri;
+import models.libri.Libro;
 
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AggiuntaLibroController extends BaseController implements Initializable {
 
@@ -18,6 +28,10 @@ public class AggiuntaLibroController extends BaseController implements Initializ
     private TextField copieDisponibili;
     @FXML
     private TextField ISBN;
+    @FXML
+    private TextField annoPubblicazione;
+
+
 
 
     @Override
@@ -26,11 +40,45 @@ public class AggiuntaLibroController extends BaseController implements Initializ
     }
 
     @FXML
-    private void onConfirm(){
+    private void onConfirm() {
         // Confirmation logic
+        String t = titolo.getText();
+        String a = autori.getText();
+        String isbn = ISBN.getText();
+        List<Autore> al = new ArrayList<Autore>();
+        String[] x = a.split(",");
 
+        for (int i = 0; i < x.length; i++) {
+            String[] y = x[i].split(" ");
+            String nome = y[0];
+            String cognome = y[1];
+            al.add(new Autore(nome, cognome));
+        }
+        try {
 
-        
+            int c = Integer.parseInt(copieDisponibili.getText());
+            int anno = Integer.parseInt(annoPubblicazione.getText());
+
+            Libro.verificaLibro(al, t, anno, isbn, c);
+
+        }catch(NumberFormatException e) {
+            Logger.getLogger(AggiuntaLibroController.class.getName()).log(Level.SEVERE, null, e);
+        }catch(RuntimeException e) {
+            Logger.getLogger(AggiuntaLibroController.class.getName()).log(Level.SEVERE, null, e);
+        }
+        Libro l= new Libro(t,Integer.parseInt(annoPubblicazione.getText()),new ISBN(isbn),Integer.parseInt(copieDisponibili.getText()),al);
+
+        try {
+           biblioteca.getLibri().aggiungi(l);
+        }catch(Exception e) {
+            Logger.getLogger(AggiuntaLibroController.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        LibriController libriController = (LibriController) parentController;
+        libriController.addBooks();
+        Stage stage = (Stage) titolo.getScene().getWindow();
+        stage.close();
+
     }
 
     @FXML
