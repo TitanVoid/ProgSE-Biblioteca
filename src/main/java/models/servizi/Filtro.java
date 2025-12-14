e152e0abfe864d57bc08abdeef08deeca76c119c
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-14 16:02:19 +0100

    Fix commenti Doxygen in Filtro.java

--- src/main/java/models/servizi/Filtro.java
+++ src/main/java/models/servizi/Filtro.java
@@ -2,10 +2,17 @@ package models.servizi;
 
 /**
  * @class Filtro
- * @brief Tipi di filtro disponibili.
- * - TUTTI: Mostra tutti gli elementi.
- * - ATTIVI: Mostra solo gli elementi attivi.
- * - CONCLUSI: Mostra solo gli elementi conclusi.
+ * 
+ * @brief Classe che definisce le tipologie di filtro disponibili.
+ * 
+ *        Utilizzata in Prestiti, permette di filtrare l'archivio dei prestiti
+ *        secondo tre criteri di visualizzazione:
+ * 
+ *        - TUTTI: Filtro che mostra tutti i prestiti presenti nell'archivio;
+ *        - ATTIVI: Filtro che mostra soltanto i prestiti attivi;
+ *        - CONCLUSI: Filtro che mostra soltanto i prestiti conclusi.
+ * 
+ * @see Prestiti
  */
 public enum Filtro {
     TUTTI,
