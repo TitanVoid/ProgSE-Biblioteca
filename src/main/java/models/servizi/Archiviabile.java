1e45978088bd8ee3847242f5c53cb9148193d568
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-14 15:34:00 +0100

    Fix commenti Doxygen in Archiviabile.java

--- src/main/java/models/servizi/Archiviabile.java
+++ src/main/java/models/servizi/Archiviabile.java
@@ -4,31 +4,49 @@ import models.OggettoGiaPresenteException;
 
 /**
  * @interface Archiviabile
- * @brief Interfaccia per la gestione di una collezione di elementi.
- *
- * @tparam E Tipo degli elementi della collezione.
+ * 
+ * @brief Interfaccia generica per la gestione di un archivio di elementi.
+ * 
+ *        L'interfaccia definisce le seguenti operazioni per la manipolazione di
+ *        un archivio di elementi: aggiunta, rimozione e modifica.
+ * 
+ * @param <E> Tipo degli elementi contenuti nell'archivio.
  */
-public interface Archiviabile<E>{
+public interface Archiviabile<E> {
 
     /**
-     * @brief Aggiunge un elemento all'archivio.
-     * @param[in] elemento L'elemento da aggiungere.
-     * @return La lista aggiornata degli elementi.
+     * @brief Aggiunge un nuovo elemento all'archivio.
+     * 
+     * @post L'archivio conterrà l'elemento passato come parametro.
+     * 
+     * @param[in] elemento Elemento da aggiungere.
+     * 
+     * @throws OggettoGiaPresenteException nel caso in cui si provi ad aggiungere un
+     *                                     elemento già presente nell'archivio.
      */
     public void aggiungi(E elemento) throws OggettoGiaPresenteException;
 
     /**
      * @brief Rimuove un elemento dall'archivio.
-     * @param[in] elemento L'elemento da rimuovere.
-     * @return La lista aggiornata degli elementi.
+     * 
+     *        Questo metodo segna un elemento come rimosso dall'archivio.
+     *        In base alla collezione da gestire, può trattarsi sia di una rimozione
+     *        logica che di una fisica.
+     * 
+     * @post L'elemento passato come parametro sarà rimosso con successo
+     *       dall'archivio.
+     * 
+     * @param[in] elemento Elemento da rimuovere.
      */
     public void rimuovi(E elemento);
 
     /**
-     * @brief Modifica un elemento nell'archivio.
-     * @param[in] originale L'elemento da modificare.
-     * @param[in] modificato L'elemento modificato.
-     * @return La lista aggiornata degli elementi.
+     * @brief Modifica un elemento presente nell'archivio.
+     * 
+     * @post L'elemento viene aggiornato all'interno dell'archivio con successo.
+     * 
+     * @param[in] originale Elemento da modificare.
+     * @param[in] modificato Elemento modificato.
      */
     public void modifica(E originale, E modificato);
 
