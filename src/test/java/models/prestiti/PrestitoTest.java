baf2015fa1c1fe7a5728791c190ea0e891c39722
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-15 06:02:37 +0100

    Separazione UTC + revisione PrestitoTest.java

--- src/test/java/models/prestiti/PrestitoTest.java
+++ src/test/java/models/prestiti/PrestitoTest.java
@@ -20,28 +20,37 @@ public class PrestitoTest {
     public void setUp() {
         Matricola m = new Matricola("0612708796");
         ISBN isbn = new ISBN("883010471X");
+        
         p = new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(3));
     }
 
+    // TEST COSTRUTTORE
     @Test
     public void testCostruttorePrestito() {
-        Prestito p = new Prestito(new Matricola("0612708796"), new ISBN("883010471X"), LocalDate.now(), LocalDate.now().plusDays(3));
-        assertNotNull(p);
-        assertEquals(new Matricola("0612708796"), p.getMatricolaUtente());
-        assertEquals(new ISBN("883010471X"), p.getCodiceISBNLibro());
-        assertEquals(LocalDate.now(), p.getDataInizio());
-        assertEquals(LocalDate.now().plusDays(3), p.getDataScadenza());
+        Prestito prestito = new Prestito(new Matricola("0612708796"), new ISBN("883010471X"), LocalDate.now(), LocalDate.now().plusDays(3));
+        
+        assertNotNull(prestito);
+        
+        assertEquals(new Matricola("0612708796"), prestito.getMatricolaUtente());
+        assertEquals(new ISBN("883010471X"), prestito.getCodiceISBNLibro());
+        assertEquals(LocalDate.now(), prestito.getDataInizio());
+        assertEquals(LocalDate.now().plusDays(3), prestito.getDataScadenza());
     }
 
+    // TEST METODI GETTER
     @Test
     public void testGetMatricolaUtente() {
         Matricola m = new Matricola("0612708796");
+        
+        assertNotNull(m);
         assertEquals(m, p.getMatricolaUtente());
     }
 
     @Test
     public void testGetCodiceISBNLibro() {
         ISBN isbn = new ISBN("883010471X");
+        
+        assertNotNull(isbn);
         assertEquals(isbn, p.getCodiceISBNLibro());
     }
 
@@ -56,10 +65,18 @@ public class PrestitoTest {
     }
 
     @Test
-    public void testGetDataRestituzione() {
+    public void testGetDataRestituzioneAttivo() {
         assertNull(p.getDataRestituzione());
     }
+    
+    @Test
+    public void testGetDataRestituzioneConcluso() {
+        p.setDataRestituzione(LocalDate.now());
+        
+        assertEquals(LocalDate.now(), p.getDataRestituzione());
+    }    
 
+    // TEST METODI SETTER
     @Test
     public void testSetDataScadenza() {
         p.setDataScadenza(LocalDate.now().plusDays(10));
@@ -74,50 +91,75 @@ public class PrestitoTest {
         assertEquals(LocalDate.now(), p.getDataRestituzione());
     }
 
+    // TEST ISATTIVO
     @Test
-    public void testIsAttivo() {
+    public void testIsAttivoTrue() {
         Matricola m = new Matricola("0612708796");
         ISBN isbn = new ISBN("883010471X");
-        // Prestito attivo:
+
         Prestito p1 = new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(3));
-        // Prestito estinto:
+
+        assertTrue(p1.isAttivo());
+    }
+    
+    @Test
+    public void testIsAttivoFalse() {
+        Matricola m = new Matricola("0612708796");
+        ISBN isbn = new ISBN("883010471X");
+
         Prestito p2 = new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(3));
         p2.setDataRestituzione(LocalDate.now());
 
-        assertTrue(p1.isAttivo());
         assertFalse(p2.isAttivo());
     }
 
+    // TEST VERIFICA DATA SCADENZA
     @Test
-    public void testVerificaDataScadenza() {
-        // Test con data di scadenza valida:
-        assertTrue(Prestito.verificaDataScadenza("2025-12-20"));
-        // Test con data di scadenza non valida:
+    public void testVerificaDataScadenzaValida() {
+        assertTrue(Prestito.verificaDataScadenza("2026-12-20"));
+    }
+    
+    @Test
+    public void testVerificaDataScadenzaFormatoNonValido() {
         assertThrows(FormatoCampiErratoException.class, () -> {
             Prestito.verificaDataScadenza("2025/12/20");
         });
+    }
+    
+    @Test
+    public void testVerificaDataScadenzaDataNonValida() {
+        // Data antecedente alla data odierna:
         assertThrows(FormatoCampiErratoException.class, () -> {
             Prestito.verificaDataScadenza("2025-12-12");
         });
-        // Test con formato corretto ma data non valida:
+        // Data non valida:
         assertThrows(DateTimeException.class, () -> {
             Prestito.verificaDataScadenza("2025-13-50");
         });
     }
 
+    // TEST COMPARETO
     @Test
-    public void testCompareTo() {
+    public void testCompareToUguale() {
         Matricola m = new Matricola("0612708796");
         ISBN isbn = new ISBN("883010471X");
 
-        // Test prestito con data di scadenza uguale:
-        Prestito p1 = new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(3));
-        assertEquals(0, p.compareTo(p1));
-        // Test prestito con data di scadenza precedente:
-        Prestito p2 = new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(2));
-        assertTrue(p.compareTo(p2) > 0);
-        // Test prestito con data di scadenza successiva:
-        Prestito p3 = new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(4));
-        assertTrue(p.compareTo(p3) < 0);
+        assertEquals(0, p.compareTo(new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(3))));
+    }
+    
+    @Test
+    public void testCompareToMinore() {
+        Matricola m = new Matricola("0612708796");
+        ISBN isbn = new ISBN("883010471X");
+
+        assertTrue(p.compareTo(new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(4))) < 0);
+    }
+    
+    @Test
+    public void testCompareToMaggiore() {
+        Matricola m = new Matricola("0612708796");
+        ISBN isbn = new ISBN("883010471X");
+
+        assertTrue(p.compareTo(new Prestito(m, isbn, LocalDate.now(), LocalDate.now().plusDays(2))) > 0);
     }
 }
