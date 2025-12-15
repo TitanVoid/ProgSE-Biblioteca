e3f8391d299bf1d5b6403689a1dddd026d8212d3
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-15 04:43:54 +0100

    Separazione + revisione UTC

--- src/test/java/models/libri/LibriTest.java
+++ src/test/java/models/libri/LibriTest.java
@@ -23,32 +23,43 @@ public class LibriTest {
         libri.aggiungi(new Libro("La Fattoria degli Animali", 1945, new ISBN("888337908X"), 8, "George Orwell")); 
     }
     
+    // TEST COSTRUTTORE
     @Test
-    public void testCostruttoreLibri() {
+    public void testCostruttoreLibriInizializzazione() {
         Libri libri2 = new Libri();
         
+        assertNotNull(libri2);
         assertNotNull(libri2.getListaLibri());
-        assertTrue(libri2.getListaLibri().isEmpty());
     }
     
     @Test
+    public void testCostruttoreLibriListaVuota() {
+        Libri libri2 = new Libri();
+                
+        assertTrue(libri2.getListaLibri().isEmpty());
+    }
+    
+    // TEST METODO GETTER
+    @Test
     public void testGetListaLibri() {
+        assertNotNull(libri.getListaLibri());
         assertTrue(libri.getListaLibri() instanceof ArrayList);
     }
 
+    // TEST RICERCA
     @Test
     public void testRicercaLibri() {
         List<Libro> risultati;
-        risultati = libri.ricercaLibri("ORWELL");
+        risultati = libri.ricercaLibri("OrWeLl");
         
         assertTrue(risultati.contains(libri.ottieni(new ISBN("8883379071"))));
         assertTrue(risultati.contains(libri.ottieni(new ISBN("888337908X"))));
         assertEquals(2, risultati.size());
     }
 
+    // TEST AGGIUNTA
     @Test
-    public void testAggiungi() throws Exception {
-        // Test libro nuovo:
+    public void testAggiungiNuovo() throws Exception {
         ISBN i = new ISBN("8807900122");
         Libro l = new Libro("Cime Tempestose", 1847, i, 9, "Emily Brontë");
         
@@ -56,11 +67,14 @@ public class LibriTest {
         
         assertTrue(libri.getListaLibri().contains(l));
         assertTrue(libri.esisteChiave(i));
-        
-        // Test libro duplicato:
-        assertThrows(LibroGiaPresenteException.class, () -> { libri.aggiungi(l); });
     }
 
+    @Test
+    public void testAggiungiDuplicato() throws Exception {
+        assertThrows(LibroGiaPresenteException.class, () -> { libri.aggiungi(new Libro("1984", 1948, new ISBN("8883379071"), 11, "George Orwell")); });
+    }
+    
+    // TEST MODIFICA
     @Test
     public void testModifica() {
         ISBN i = new ISBN("8883379071");
@@ -73,6 +87,7 @@ public class LibriTest {
         assertEquals(20, libri.ottieni(i).getCopieDisponibili());
     }
 
+    // TEST RIMUOVI
     @Test
     public void testRimuovi() {
         ISBN i = new ISBN("8883379071");
@@ -84,17 +99,22 @@ public class LibriTest {
         assertFalse(libri.esisteChiave(i));        
     }
 
+    // TEST ESISTECHIAVE
     @Test
-    public void testEsisteChiave() {
+    public void testEsisteChiaveTrue() {
         assertTrue(libri.esisteChiave(new ISBN("883010471X")));
         assertTrue(libri.esisteChiave(new ISBN("8883379071")));
-        assertTrue(libri.esisteChiave(new ISBN("888337908X")));
-        
-        assertFalse(libri.esisteChiave(new ISBN("8807900122")));   
+        assertTrue(libri.esisteChiave(new ISBN("888337908X"))); 
     }
-
+    
     @Test
-    public void testOttieni() {
+    public void testEsisteChiaveFalse() {
+        assertFalse(libri.esisteChiave(new ISBN("8807900122")));   
+    }    
+
+    // TEST OTTIENI
+    @Test
+    public void testOttieniLibroPresente() {
         ISBN i1 = new ISBN("883010471X");
         ISBN i2 = new ISBN("8883379071");
         ISBN i3 = new ISBN("888337908X");
@@ -102,13 +122,17 @@ public class LibriTest {
         Libro l1 = new Libro("Il Signore degli Anelli", 1954, i1, 5, "J.R.R. Tolkien");
         Libro l2 = new Libro("1984", 1948, i2, 11, "George Orwell");
         Libro l3 = new Libro("La Fattoria degli animali", 1945, i3, 8, "George Orwell");
-        
-        ISBN i4 = new ISBN("8807900122");
-        
+                
         assertEquals(libri.ottieni(i1), l1);
         assertEquals(libri.ottieni(i2), l2);
         assertEquals(libri.ottieni(i3), l3);
-        assertNull(libri.ottieni(i4));
     }
+    
+    @Test
+    public void testOttieniLibroAssente() {
+        ISBN i4 = new ISBN("8807900122");
+        
+        assertNull(libri.ottieni(i4));
+    }    
     
 }
