789bf1901507dc803ec9a214fc6251d276f58bf1
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-15 05:28:43 +0100

    Separazione UTC + revisione UtentiTest.java

--- src/test/java/models/utenti/UtentiTest.java
+++ src/test/java/models/utenti/UtentiTest.java
@@ -18,36 +18,48 @@ public class UtentiTest {
     public void setUp() throws Exception {
         utenti = new Utenti();
 
-        utenti.aggiungi(new Utente("Luisa", "Genovese", new Matricola("0612708796"), "l.genovese20@studenti.unisa.it"));
+        utenti.aggiungi(new Utente("Luisa", "Genovese", new Matricola("0612708796"), "l.genovese@studenti.unisa.it"));
         utenti.aggiungi(new Utente("Erica", "Brancaccio", new Matricola("0612709465"), "e.brancaccio@studenti.unisa.it"));
         utenti.aggiungi(new Utente("Francesco", "Altieri", new Matricola("0612712649"), "f.altieri@studenti.unisa.it"));
         utenti.aggiungi(new Utente("Paolo", "Alfe", new Matricola("0612708112"), "p.alfe@studenti.unisa.it"));
     }
 
+    // TEST COSTRUTTORE
     @Test
-    public void testCostruttoreUtenti() {
+    public void testCostruttoreUtentiInizializzazioneLista() {
         Utenti utenti2 = new Utenti();
 
+        assertNotNull(utenti2);
+        
         assertNotNull(utenti2.getListaUtenti());
-        assertTrue(utenti2.getListaUtenti().isEmpty());
     }
+    
+    @Test
+    public void testCostruttoreUtentiListaVuota() {
+        Utenti utenti2 = new Utenti();
 
+        assertTrue(utenti2.getListaUtenti().isEmpty());
+    }    
+
+    // TEST METODO GETTER
     @Test
     public void testGetListaUtenti() {
+        assertNotNull(utenti.getListaUtenti());
         assertTrue(utenti.getListaUtenti() instanceof ArrayList);
     }
 
+    // TEST RICERCA UTENTI
     @Test
     public void testRicercaUtenti() {
-        List<Utente> risultati = utenti.ricercaUtenti("GENOVESE");
+        List<Utente> risultati = utenti.ricercaUtenti("GeNoVeSe");
 
         assertTrue(risultati.contains(utenti.ottieni(new Matricola("0612708796"))));
         assertEquals(1, risultati.size());
     }
 
+    // TEST AGGIUNGI
     @Test
-    public void testAggiungi() throws Exception {
-        // Test utente nuovo:
+    public void testAggiungiNuovo() throws Exception {
         Matricola m = new Matricola("0612712345");
         Utente u = new Utente("Mario", "Rossi", m, "m.rossi10@studenti.unisa.it");
 
@@ -55,17 +67,20 @@ public class UtentiTest {
 
         assertTrue(utenti.getListaUtenti().contains(u));
         assertTrue(utenti.esisteChiave(m));
-
-        // Test utente duplicato:
+    }
+    
+    @Test
+    public void testAggiungiDuplicato() throws Exception {
         assertThrows(UtenteGiaPresenteException.class, () -> {
-            utenti.aggiungi(u);
-        });
+            utenti.aggiungi(new Utente("Luisa", "Genovese", new Matricola("0612708796"), "l.genovese@studenti.unisa.it"));
+        }); 
     }
 
+    // TEST RIMUOVI
     @Test
     public void testRimuovi() {
         Matricola m = new Matricola("0612708796");
-        Utente u = new Utente("Luisa", "Genovese", m, "l.genovese20@studenti.unisa.it");
+        Utente u = new Utente("Luisa", "Genovese", m, "l.genovese@studenti.unisa.it");
 
         utenti.rimuovi(u);
 
@@ -73,47 +88,57 @@ public class UtentiTest {
         assertFalse(utenti.esisteChiave(m));
     }
 
+    // TEST MODIFICA
     @Test
     public void testModifica() {
         Matricola m = new Matricola("0612708796");
 
-        Utente u = new Utente("Luisa", "Genovese", m, "l.genovese20@studenti.unisa.it");
-        u.setEmail("l.genovese@studenti.unisa.it");
+        Utente u = new Utente("Luisa", "Genovese", m, "l.genovese@studenti.unisa.it");
+        u.setEmail("l.genovese20@studenti.unisa.it");
 
         utenti.modifica(utenti.ottieni(m), u);
 
-        assertEquals("l.genovese@studenti.unisa.it", utenti.ottieni(m).getEmail());
+        assertEquals("l.genovese20@studenti.unisa.it", utenti.ottieni(m).getEmail());
     }
 
+    // TEST ESISTECHIAVE
     @Test
-    public void testEsisteChiave() {
+    public void testEsisteChiaveTrue() {
         assertTrue(utenti.esisteChiave(new Matricola("0612708796")));
         assertTrue(utenti.esisteChiave(new Matricola("0612709465")));
         assertTrue(utenti.esisteChiave(new Matricola("0612712649")));
         assertTrue(utenti.esisteChiave(new Matricola("0612708112")));
-
-        assertFalse(utenti.esisteChiave(new Matricola("0612712345")));
     }
-
+    
     @Test
-    public void testOttieni() {
+    public void testEsisteChiaveFalse() {
+        assertFalse(utenti.esisteChiave(new Matricola("0612712345")));
+    }   
+
+    // TEST OTTIENI
+    @Test
+    public void testOttieniUtentePresente() {
         Matricola m1 = new Matricola("0612708796");
         Matricola m2 = new Matricola("0612709465");
         Matricola m3 = new Matricola("0612712649");
         Matricola m4 = new Matricola("0612708112");
-
+        
         Utente u1 = new Utente("Luisa", "Genovese", m1, "l.genovese20@studenti.unisa.it");
         Utente u2 = new Utente("Erica", "Brancaccio", m2, "e.brancaccio@studenti.unisa.it");
         Utente u3 = new Utente("Francesco", "Altieri", m3, "f.altieri@studenti.unisa.it");
         Utente u4 = new Utente("Paolo", "Alfe", m4, "p.alfe@studenti.unisa.it");
-
-        Matricola m5 = new Matricola("0612712345");
-
+                
         assertEquals(u1, utenti.ottieni(m1));
         assertEquals(u2, utenti.ottieni(m2));
         assertEquals(u3, utenti.ottieni(m3));
         assertEquals(u4, utenti.ottieni(m4));
-
-        assertNull(utenti.ottieni(m5));
     }
+    
+    @Test
+    public void testOttieniUtenteAssente() {
+        Matricola m5 = new Matricola("0612712345");
+        
+        assertNull(utenti.ottieni(m5));
+    }       
+
 }
