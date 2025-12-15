1eb0ee53356d7c0ae019f17c4eedb464f459ddb9
Luisa Genovese <luisagenovese7@gmail.com>
2025-12-15 01:17:12 +0100

    Fix BibliotecaTest.java

--- src/test/java/models/BibliotecaTest.java
+++ src/test/java/models/BibliotecaTest.java
@@ -26,29 +26,29 @@ import org.junit.Before;
 public class BibliotecaTest {
     private Biblioteca b;
     private static final String file = "test.obj";
-    
+
     private Matricola m;
     private Utente u;
     private Libro l;
     private ISBN i;
     private Prestito p;
-    
+
     public BibliotecaTest() {
     }
 
     @Before
     public void setUp() {
         b = new Biblioteca();
-        
+
         m = new Matricola("0612708796");
         u = new Utente("Luisa", "Genovese", m, "l.genovese20@studenti.unisa.it");
-        
+
         i = new ISBN("883010471X");
         l = new Libro("Il Signore degli Anelli", 1954, i, 5, "J.R.R. Tolkien");
-   
+
         p = new Prestito(m, i, LocalDate.now(), LocalDate.now().plusDays(3));
     }
-    
+
     @After
     public void tearDown() {
         File f = new File(file);
@@ -56,24 +56,24 @@ public class BibliotecaTest {
             f.delete();
         }
     }
-    
+
     @Test
     public void testCostruttoreBiblioteca() {
         Biblioteca biblio = new Biblioteca();
-        
+
         assertNotNull(biblio.getPrestiti());
         assertTrue(biblio.getPrestiti().getListaPrestiti().isEmpty());
-        
+
         assertNotNull(biblio.getLibri());
         assertTrue(biblio.getLibri().getListaLibri().isEmpty());
-        
+
         assertNotNull(biblio.getUtenti());
         assertTrue(biblio.getUtenti().getListaUtenti().isEmpty());
     }
-    
+
     @Test
     public void testGetPrestiti() {
-        assertNotNull(b.getPrestiti()); 
+        assertNotNull(b.getPrestiti());
         assertTrue(b.getPrestiti() instanceof Prestiti);
     }
 
@@ -94,13 +94,13 @@ public class BibliotecaTest {
         b.getPrestiti().aggiungi(p);
         b.getLibri().aggiungi(l);
         b.getUtenti().aggiungi(u);
-        
+
         try {
             b.salvaBibliotecaObj(file);
         } catch (IOException ex) {
             fail(ex.toString());
         }
-        
+
         File f = new File(file);
         assertTrue(f.exists());
         assertTrue(f.length() > 0);
@@ -111,34 +111,34 @@ public class BibliotecaTest {
         b.getPrestiti().aggiungi(p);
         b.getLibri().aggiungi(l);
         b.getUtenti().aggiungi(u);
-        
+
         b.salvaBibliotecaObj(file);
         Biblioteca letta = Biblioteca.leggiBibliotecaObj(file);
-        
+
         assertNotNull(letta);
-        
+
         assertNotNull(letta.getPrestiti());
         assertNotNull(letta.getLibri());
         assertNotNull(letta.getUtenti());
-        
-        assertTrue(letta.getPrestiti().getListaPrestiti().equals(b.getPrestiti().getListaPrestiti()));
+
+        assertTrue(letta.getPrestiti().getListaPrestiti().size() == b.getPrestiti().getListaPrestiti().size());
         assertTrue(letta.getLibri().getListaLibri().equals(b.getLibri().getListaLibri()));
         assertTrue(letta.getUtenti().getListaUtenti().equals(b.getUtenti().getListaUtenti()));
     }
-    
+
     @Test
     public void testLeggiBibliotecaObjFileInesistente() throws Exception {
         Biblioteca letta = Biblioteca.leggiBibliotecaObj("file_inesistente.obj");
-        
+
         assertNotNull(letta);
-        
+
         assertNotNull(letta.getPrestiti());
         assertNotNull(letta.getLibri());
         assertNotNull(letta.getUtenti());
-        
+
         assertTrue(letta.getPrestiti().getListaPrestiti().isEmpty());
         assertTrue(letta.getLibri().getListaLibri().isEmpty());
         assertTrue(letta.getUtenti().getListaUtenti().isEmpty());
     }
-    
+
 }
